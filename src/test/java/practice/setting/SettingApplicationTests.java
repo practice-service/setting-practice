b08dc5561db3dc7d94f52a9e2e9cb66640184964
iiqcov <iiqcov@gmail.com>
2024-09-10 22:50:10 +0900

    initial commit

---
+++ src/test/java/practice/setting/SettingApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.setting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
