163a27a8ba7b884e64e8747d3217f9a7f4bb6ff1
iiqcov <iiqcov@gmail.com>
2024-09-16 00:12:30 +0900

    test : 테스트 코드 작성

--- src/test/java/practice/setting/SettingApplicationTests.java
+++ src/test/java/practice/setting/SettingApplicationTests.java
@@ -2,8 +2,10 @@ package practice.setting;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SettingApplicationTests {
 
 	@Test
