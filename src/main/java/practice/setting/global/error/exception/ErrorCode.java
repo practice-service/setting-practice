962fd9fb4b0eda974aba1c0db052c62c7787d9c7
iiqcov <iiqcov@gmail.com>
2024-09-22 01:11:14 +0900

    chore : 커스텀 에러 설정

---
+++ src/main/java/practice/setting/global/error/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package practice.setting.global.error.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    SAMPLE_ERROR(HttpStatus.BAD_REQUEST, "Sample Error Message"),
+    ;
+
+    private final HttpStatus status;
+    private final String message;
+}
